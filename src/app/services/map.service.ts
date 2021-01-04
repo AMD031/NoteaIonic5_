13c422500c36e54aae921f4c347c743913443d68
A.M.D <teclado1333@gmail.com>
2021-01-04 18:13:23 +0100

    icono mapa

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Map, latLng, tileLayer, Layer, marker, LayerGroup, layerGroup } from 'leaflet';
+import { Map, latLng, tileLayer, Layer, marker, LayerGroup, layerGroup , icon } from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,24 @@ export class MapService {
 
   private map: Map = null;
   private markerGroup: LayerGroup = null;
+ 
+  // private icono = icon({
+  //   iconUrl: 'assets/img/ubicacion.svg',
+  //   iconSize: [15, 15], 
+  //   popupAnchor: [0, -20]
+  // });
+
+  private icono = icon({
+    iconUrl: 'assets/img/ubicacion.svg',
+    // shadowUrl: 'assets/img/ubicacion.svg',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
   constructor() { }
 
   leafletMap(lat: number, long: number, id: string = 'mapId') {
@@ -43,7 +61,7 @@ export class MapService {
         this.markerGroup.clearLayers();
       }
       this.markerGroup = layerGroup(null).addTo(this.map);
-      marker([lat, long])
+      marker([lat, long], {icon: this.icono})
         .addTo(this.markerGroup)
         .bindPopup(lat + ' ' + long)
         .openPopup();
